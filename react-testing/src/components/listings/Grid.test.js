9c2524191cbd2c0cfa6b298ec73115c6b4024343
shreelimbkar <limbkarshree@gmail.com>
2018-03-23 14:52:46 +0530

    organize tests with describe
    organize tests with describe

--- react-testing/src/components/listings/Grid.test.js
+++ react-testing/src/components/listings/Grid.test.js
@@ -2,35 +2,44 @@ import movies from '../../data/movies.json';
 
 // Numbers
 const noOfMovies = movies.length;
-test('Number of items = 40', () => {
+describe('Numbers tests', () => {
+    test('Number of items = 40', () => {
     expect(noOfMovies).toBe(40);
-});
+    });
 
-test('Number of items greater than 40', () => {
-    expect(noOfMovies).toBeGreaterThanOrEqual(40);
+    test('Number of items greater than 40', () => {
+        expect(noOfMovies).toBeGreaterThanOrEqual(40);
+    });
 });
 
 // Strings
 const movieTitle = movies[0].Title;
-test('The title has Harry', () => {
+describe('Strings tests', () => {
+    test('The title has Harry', () => {
     expect(movieTitle).toMatch(/Harry/);
-});
+    });
 
-test('The title contains Secrets', () => {
-    expect(movieTitle).toContain('Secrets');
+    test('The title contains Secrets', () => {
+        expect(movieTitle).toContain('Secrets');
+    });
 });
 
 // Arrays
 const data2 = ['Harry Potter', 'Cars 2'];
-test('The list movies mentions Harry Potter and Cars 2', () => {
-    expect(['Harry Potter', 'Cars 2', 'Despicable']).toEqual(expect.arrayContaining(data2));
+describe('Arrays tests',() => {
+    test('The list movies mentions Harry Potter and Cars 2', () => {
+        expect(['Harry Potter', 'Cars 2', 'Despicable']).toEqual(expect.arrayContaining(data2));
+    });
 });
 
 // Object
-test('The first movie to have property of Title', () => {
-    expect(movies[0]).toHaveProperty('Title');
+describe('Object tests', () => {
+    test('The first movie to have property of Title', () => {
+        expect(movies[0]).toHaveProperty('Title');
+    });
+
+    test('The first movie to have imdbRating 7.4', () => {
+        expect(movies[0]).toHaveProperty('imdbRating', '7.4');
+    });
 });
 
-test('The first movie to have imdbRating 7.4', () => {
-    expect(movies[0]).toHaveProperty('imdbRating', '7.4');
-});
